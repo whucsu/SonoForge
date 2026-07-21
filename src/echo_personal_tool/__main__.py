b31d548f47bf42e4a0490996bf7318151926a631
areatu <kuvilkin@gmail.com>
2026-07-21 18:02:03 +0300

    ci: fix lint errors and add libegl1 for PySide6

--- src/echo_personal_tool/__main__.py
+++ src/echo_personal_tool/__main__.py
@@ -1,11 +1,10 @@
 """Application entry point."""
 
 import multiprocessing
-import sys
 
 multiprocessing.freeze_support()
 
-from echo_personal_tool.main import main
+from echo_personal_tool.main import main  # noqa: E402
 
 if __name__ == "__main__":
     raise SystemExit(main())
